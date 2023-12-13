4a5ccf31b4b0f7cad57fee88e744863491576580
Connor Swanson <swanscon@gmail.com>
2023-12-13 12:11:05 -0600

    Micrometer observation and orderPlacedEvent in OrderService

--- order-service/src/main/java/com/astontech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/astontech/orderservice/service/OrderService.java
@@ -3,14 +3,16 @@ package com.astontech.orderservice.service;
 import com.astontech.orderservice.dto.InventoryResponse;
 import com.astontech.orderservice.dto.OrderLineItemsDto;
 import com.astontech.orderservice.dto.OrderRequest;
+import com.astontech.orderservice.event.OrderPlacedEvent;
 import com.astontech.orderservice.model.Order;
 import com.astontech.orderservice.model.OrderLineItems;
 import com.astontech.orderservice.repository.OrderRepository;
-import io.micrometer.tracing.Span;
-import io.micrometer.tracing.Tracer;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -26,7 +28,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-    private final Tracer tracer;
+    private final ObservationRegistry observationRegistry;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -42,28 +45,28 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
-
-        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
-            //Call Inventory Service, and place order if product is in stock
+        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup",
+                this.observationRegistry);
+        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
+        return inventoryServiceObservation.observe(() -> {
             InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder
-                        .queryParam("skuCode", skuCodes).build())
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
+
             boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
-                .allMatch(InventoryResponse::isInStock);
+                    .allMatch(InventoryResponse::isInStock);
+
             if(allProductsInStock) {
                 orderRepository.save(order);
-                return "Order Placed Successfully";
+                applicationEventPublisher.publishEvent(new OrderPlacedEvent(this, order.getOrderNumber()));
+                return "Order Placed";
             } else {
-                throw new IllegalArgumentException("Product not in stock.");
+                throw new IllegalArgumentException("Product is not in stock, please try again later.");
             }
-        } finally {
-            inventoryServiceLookup.end();
-        }
-
+        });
 
 
     }
